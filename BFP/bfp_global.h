cf631ddc66dc3ba88064bb050c23d52dbcbaaca0
AzuxDario <pawel_mibor95@tlen.pl>
2018-04-24 23:06:43 +0200

    Remove unused defines

--- BFP/bfp_global.h
+++ BFP/bfp_global.h
@@ -1,11 +1,6 @@
 #ifndef TESTLIBRART_GLOBAL_H
 #define TESTLIBRART_GLOBAL_H
 
-#define signPosition                0
-#define exponentLeftPosition        1
-#define exponentRightPosition       20
-#define significantLeftPosition     21
-#define significantRightPosition    255
 #define bfpSignificantArraySize     28
 
 
