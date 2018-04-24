c7ff9d7e01eb6e083eb0c7a5830c42962d4a55b1
AzuxDario <pawel_mibor95@tlen.pl>
2018-04-24 23:25:27 +0200

    Move lifes and add dosumantation

---
+++ BFP/Arthmetic/Arthmetic.pri
@@ -0,0 +1,8 @@
+INCLUDEPATH += Arthmetic
+DEPENDPATH += Arthmetic
+SOURCES += \
+	$$PWD/absbfp.c \
+	$$PWD/negBfp.c
+
+HEADERS += \ 
+    $$PWD/arthmetic.h
---
+++ BFP/Arthmetic/arthmetic.h
@@ -0,0 +1,16 @@
+#ifndef ARTHMETIC_H
+#define ARTHMETIC_H
+#include "BFP.h"
+
+//! Get absolute value.
+/*!
+  \param value pointer to value to make absolute.
+*/
+void absBfp(bfp* value);
+
+//! Negate value.
+/*!
+  \param value pointer to value to negate.
+*/
+void negBfp(bfp* value);
+#endif // ARTHMETIC_H
