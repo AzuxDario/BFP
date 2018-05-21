ab8eaae93339da130ac1a8ae7998d216c128e8ee
AzuxDario <pawel_mibor95@tlen.pl>
2018-05-21 15:50:07 +0200

    Change in initZero function

--- BFP/Initialization/initZero.c
+++ BFP/Initialization/initZero.c
@@ -5,6 +5,7 @@ void initZero(bfp* value)
     int i;
     value->sign = 0;
     value->exponent = 0;
+    value->separatorPlace = 1;
     for(i = 0; i < bfpSignificantArraySize; i++)
     {
         value->significant[i] = 0;
