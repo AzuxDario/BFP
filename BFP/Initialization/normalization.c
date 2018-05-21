9080060ae4d178f642614a10ecbc44019cac2f1d
AzuxDario <pawel_mibor95@tlen.pl>
2018-05-22 00:18:17 +0200

    Fix normalization

--- BFP/Initialization/normalization.c
+++ BFP/Initialization/normalization.c
@@ -1,4 +1,5 @@
 #include "Initialization.h"
+#include <stdbool.h>
 
 void normalization(bfp* value)
 {
@@ -14,15 +15,34 @@ void normalization(bfp* value)
         value->separatorPlace = 1;
     }
 
+    // When value is 0 next loop will be repeat infinity times. To avoid it we need to catch zero.
+    bool isZero = true;
+    for(int i = 0; i < bfpSignificantArraySize - 1; i++)
+    {
+        if(value->significant[i] != 0)
+        {
+            isZero = false;
+            break;
+        }
+    }
+
+    // If we have zero value make it proper zero and leave function.
+    if(isZero == true)
+    {
+        initZero(value);
+        return;
+    }
+
     //If there are lead zeros remove them.
     while(value->significant[0] == 0)
     {
-        //Shift left.
+        // This loop is doing shift left of whole significant.
         // -1 to avoid take value out of scope.
         for(int i = 0; i < bfpSignificantArraySize - 1; i++)
         {
             value->significant[i] = value->significant[i+1];
         }
+
         //Assign last zero.
         value->significant[bfpSignificantArraySize - 1] = 0;
         value->exponent--;
