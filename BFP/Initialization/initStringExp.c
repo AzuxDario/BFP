09634da031fbaf855b0c668f6889d800a42185f9
AzuxDario <pawel_mibor95@tlen.pl>
2018-06-02 16:29:07 +0200

    Add checking NaN and Inf in initStringExp function

--- BFP/Initialization/initStringExp.c
+++ BFP/Initialization/initStringExp.c
@@ -3,5 +3,28 @@
 void initStringExp(bfp* target, char* source, int exponent)
 {
     initString(target, source);
-    target->exponent += exponent;
+    int sum = target->exponent + exponent;
+
+    // Check if we have NaN or Inf
+    if(target->exponent > 0 && exponent > 0)
+    {
+        if(sum < target->exponent && sum < exponent)
+        {
+            // NaN or Inf
+            target->separatorPlace = 1;
+            target->exponent = bfpExponentMaxValue;
+            return;
+        }
+    }
+    else if(target->exponent < 0 && exponent < 0)
+    {
+        if(sum > target->exponent && sum > exponent)
+        {
+            // NaN or Inf
+            target->separatorPlace = 1;
+            target->exponent = bfpExponentMinValue;
+            return;
+        }
+    }
+
 }
