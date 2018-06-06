51ab84aabc25638adff3c108cba9c68609477ab3
AzuxDario <pawel_mibor95@tlen.pl>
2018-06-07 01:26:24 +0200

    Changes in test project

--- TestProject/main.c
+++ TestProject/main.c
@@ -17,7 +17,7 @@ int main()
     printf("Wynik: %d\n", result);
     float value = 1.2345f;
     bfp number, number2, number3, number4, number5, number6, numberZero, add1, add2, sub1, sub2, mul1, mul2, div1, div2, pow, sqrt, fact, sin, cos, tan, ctan, exp, log, log10, ceil;
-    bfp round;
+    bfp round, floor;
     initString(&number, "12.34567890");
     initString(&number2, "0.00123");
     initStringExp(&number3, "0.00123", -1);
@@ -44,8 +44,9 @@ int main()
     initString(&log10, "10");
     initString(&ceil, "-2.7");
     initString(&round, "2.3");
+    initString(&floor, "2.3");
     char *string, *string2, *string3, *string4, *string5, *string6, *string7, *string8, *string9, *string10, *string11, *string12, *string13, *string14, *string15, *string16;
-    char *string17, *string18, *string19;
+    char *string17, *string18, *string19, *string20;
     string = toStringExp(&number6, 10);
     string2 = toString(&number6, 5);
     string3 = toString(&number2, 500);
@@ -65,6 +66,7 @@ int main()
     log10Bfp(&log10);
     ceilBfp(&ceil);
     roundBfp(&round);
+    floorBfp(&floor);
     string4 = toString(&add1, 2);
     string5 = toString(&sub1, 2);
     string6 = toString(&mul1, 6);
@@ -81,6 +83,7 @@ int main()
     string17 = toString(&log10, bfpSignificantArraySize - 1);
     string18 = toString(&ceil, 3);
     string19 = toString(&round, 3);
+    string20 = toString(&floor, 3);
     printf("%s\n", string);
     printf("%s\n", string2);
     printf("%s\n", string3);
@@ -100,5 +103,6 @@ int main()
     printf("Log10: %s\n", string17);
     printf("Ceil: %s\n", string18);
     printf("Round: %s\n", string19);
+    printf("Floor: %s\n", string20);
     printf("Done yay\n");
 }
