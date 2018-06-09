15b3cf41ee8ebd1edb2ba666963b4b4a374aa95e
AzuxDario <pawel_mibor95@tlen.pl>
2018-06-09 23:33:16 +0200

    Add checking input string

--- BFP/Initialization/initString.c
+++ BFP/Initialization/initString.c
@@ -18,28 +18,63 @@ void initString(bfp* target, char* source)
             break;
         }
         // If sing is detected
-        if(source[i] == '-')
+        if(source[i] == '-' && i == 0)
         {
             target->sign = 1;
             isSignDetected = true;
             continue;
         }
-        else if(source[i] == '+')
+        else if(source[i] == '-' && i != 0)
+        {
+            // NaN
+            target->exponent = bfpExponentMaxValue;
+            // NaN should have at least one digit different than 0
+            target->significant[0] = 1;
+            return;
+        }
+        else if(source[i] == '+' && i == 0)
         {
             target->sign = 0;
             isSignDetected = true;
             continue;
         }
+        else if(source[i] == '+' && i != 0)
+        {
+            // NaN
+            target->exponent = bfpExponentMaxValue;
+            // NaN should have at least one digit different than 0
+            target->significant[0] = 1;
+            return;
+        }
         // If under index it separator set position of separator
-        if(source[i] == '.')
+        if(source[i] == '.' && isSeparatorDetected == false)
         {
             isSeparatorDetected = true;
             target->separatorPlace = i;
             continue;
         }
+        else if(source[i] == '.' && isSeparatorDetected == true)
+        {
+            // NaN
+            target->exponent = bfpExponentMaxValue;
+            // NaN should have at least one digit different than 0
+            target->significant[0] = 1;
+            return;
+        }
         //Subtract 0x30 to change value in ascii code to number
-        target->significant[position] = source[i] - 0x30;
-        position++;
+        if(source[i] >= '0' && source[i] <= '9')
+        {
+            target->significant[position] = source[i] - 0x30;
+            position++;
+        }
+        else if(source[i] < '0' && source[i] > '9')
+        {
+            // NaN
+            target->exponent = bfpExponentMaxValue;
+            // NaN should have at least one digit different than 0
+            target->significant[0] = 1;
+            return;
+        }
     }
 
     // Subtract from separator place 1 when sign was detected
--- BFP/Initialization/initStringExp.c
+++ BFP/Initialization/initStringExp.c
@@ -1,42 +1,45 @@
 #include "Initialization.h"
+#include "comparsion.h"
 
 void initStringExp(bfp* target, char* source, int exponent)
 {
     initString(target, source);
-    int sum = target->exponent + exponent;
-
-    // Check if we have Inf
-    if(target->exponent > 0 && exponent > 0)
+    if(isNaN(target) == 0)
     {
-        if(sum < target->exponent && sum < exponent)
-        {
-            // NaN or Inf
-            target->separatorPlace = 1;
-            target->exponent = bfpExponentMaxValue;
-            // In Inf we should make sure every value will be zero
-            for(int i = 0; i < bfpSignificantArraySize; i++)
-            {
-                target->significant[i] = 0;
-            }
-            return;
-        }
-    }
-    else if(target->exponent < 0 && exponent < 0)
-    {
-        if(sum > target->exponent && sum > exponent)
-        {
-            // NaN or Inf
-            target->separatorPlace = 1;
-            target->exponent = bfpExponentMinValue;
-            // In Inf we should make sure every value will be zero
-            for(int i = 0; i < bfpSignificantArraySize; i++)
-            {
-                target->significant[i] = 0;
-            }
-            return;
-        }
-    }
+        int sum = target->exponent + exponent;
 
-    target->exponent = sum;
+        // Check if we have Inf
+        if(target->exponent > 0 && exponent > 0)
+        {
+            if(sum < target->exponent && sum < exponent)
+            {
+                // NaN or Inf
+                target->separatorPlace = 1;
+                target->exponent = bfpExponentMaxValue;
+                // In Inf we should make sure every value will be zero
+                for(int i = 0; i < bfpSignificantArraySize; i++)
+                {
+                    target->significant[i] = 0;
+                }
+                return;
+            }
+        }
+        else if(target->exponent < 0 && exponent < 0)
+        {
+            if(sum > target->exponent && sum > exponent)
+            {
+                // NaN or Inf
+                target->separatorPlace = 1;
+                target->exponent = bfpExponentMinValue;
+                // In Inf we should make sure every value will be zero
+                for(int i = 0; i < bfpSignificantArraySize; i++)
+                {
+                    target->significant[i] = 0;
+                }
+                return;
+            }
+        }
 
+        target->exponent = sum;
+    }
 }
