65745e861b6facfb9fbeaa4214ddbf4b9a17aa86
AzuxDario <pawel_mibor95@tlen.pl>
2018-08-12 19:59:34 +0200

    Create tests for comparsion

--- UnitTests/ComparsionTest/ComparsionTest.cpp
+++ UnitTests/ComparsionTest/ComparsionTest.cpp
@@ -5,7 +5,7 @@ ComparsionTest::ComparsionTest()
 
 }
 
-void ComparsionTest::isEqualBfp_Equal()
+void ComparsionTest::equalBfp_EqualPos()
 {
     bfp value1, value2;
     initInt(&value1, 10);
@@ -13,7 +13,15 @@ void ComparsionTest::isEqualBfp_Equal()
     QCOMPARE(equalBfp(&value1, &value2), true);
 }
 
-void ComparsionTest::isEqualBfp_NotEqual()
+void ComparsionTest::equalBfp_EqualNeg()
+{
+    bfp value1, value2;
+    initInt(&value1, 10);
+    initInt(&value2, 10);
+    QCOMPARE(equalBfp(&value1, &value2), true);
+}
+
+void ComparsionTest::equalBfp_NotEqualPosPos()
 {
     bfp value1, value2;
     initInt(&value1, 10);
@@ -21,7 +29,15 @@ void ComparsionTest::isEqualBfp_NotEqual()
     QCOMPARE(equalBfp(&value1, &value2), false);
 }
 
-void ComparsionTest::isEqualBfp_NanNumber()
+void ComparsionTest::equalBfp_NotEqualPosNeg()
+{
+    bfp value1, value2;
+    initInt(&value1, 10);
+    initInt(&value2, -20);
+    QCOMPARE(equalBfp(&value1, &value2), false);
+}
+
+void ComparsionTest::equalBfp_NanNumber()
 {
     bfp value1, value2;
     ValueHelper::assignNaN(&value1);
@@ -29,7 +45,7 @@ void ComparsionTest::isEqualBfp_NanNumber()
     QCOMPARE(equalBfp(&value1, &value2), false);
 }
 
-void ComparsionTest::isEqualBfp_PosInfNumber()
+void ComparsionTest::equalBfp_PosInfNumber()
 {
     bfp value1, value2;
     ValueHelper::assignPositiveInf(&value1);
@@ -37,7 +53,7 @@ void ComparsionTest::isEqualBfp_PosInfNumber()
     QCOMPARE(equalBfp(&value1, &value2), false);
 }
 
-void ComparsionTest::isEqualBfp_NegInfNumber()
+void ComparsionTest::equalBfp_NegInfNumber()
 {
     bfp value1, value2;
     ValueHelper::assignNegativeInf(&value1);
@@ -45,7 +61,7 @@ void ComparsionTest::isEqualBfp_NegInfNumber()
     QCOMPARE(equalBfp(&value1, &value2), false);
 }
 
-void ComparsionTest::isEqualBfp_NanPosInf()
+void ComparsionTest::equalBfp_NanPosInf()
 {
     bfp value1, value2;
     ValueHelper::assignNaN(&value1);
@@ -53,7 +69,7 @@ void ComparsionTest::isEqualBfp_NanPosInf()
     QCOMPARE(equalBfp(&value1, &value2), false);
 }
 
-void ComparsionTest::isEqualBfp_NanNegInf()
+void ComparsionTest::equalBfp_NanNegInf()
 {
     bfp value1, value2;
     ValueHelper::assignNaN(&value1);
@@ -61,7 +77,7 @@ void ComparsionTest::isEqualBfp_NanNegInf()
     QCOMPARE(equalBfp(&value1, &value2), false);
 }
 
-void ComparsionTest::isEqualBfp_NanNan()
+void ComparsionTest::equalBfp_NanNan()
 {
     bfp value1, value2;
     ValueHelper::assignNaN(&value1);
@@ -69,7 +85,7 @@ void ComparsionTest::isEqualBfp_NanNan()
     QCOMPARE(equalBfp(&value1, &value2), false);
 }
 
-void ComparsionTest::isEqualBfp_PosInfPosInf()
+void ComparsionTest::equalBfp_PosInfPosInf()
 {
     bfp value1, value2;
     ValueHelper::assignPositiveInf(&value1);
@@ -77,7 +93,7 @@ void ComparsionTest::isEqualBfp_PosInfPosInf()
     QCOMPARE(equalBfp(&value1, &value2), false);
 }
 
-void ComparsionTest::isEqualBfp_NegInfNegInf()
+void ComparsionTest::equalBfp_NegInfNegInf()
 {
     bfp value1, value2;
     ValueHelper::assignNegativeInf(&value1);
@@ -85,10 +101,386 @@ void ComparsionTest::isEqualBfp_NegInfNegInf()
     QCOMPARE(equalBfp(&value1, &value2), false);
 }
 
-void ComparsionTest::isEqualBfp_PosInfNegInf()
+void ComparsionTest::equalBfp_PosInfNegInf()
 {
     bfp value1, value2;
     ValueHelper::assignPositiveInf(&value1);
     ValueHelper::assignNegativeInf(&value2);
     QCOMPARE(equalBfp(&value1, &value2), false);
+}
+
+void ComparsionTest::greatherBfp_GreatherPosPos()
+{
+    bfp value1, value2;
+    initInt(&value1, 20);
+    initInt(&value2, 10);
+    QCOMPARE(greatherBfp(&value1, &value2), true);
+}
+
+void ComparsionTest::greatherBfp_GreatherPosNeg()
+{
+    bfp value1, value2;
+    initInt(&value1, 20);
+    initInt(&value2, -10);
+    QCOMPARE(greatherBfp(&value1, &value2), true);
+}
+
+void ComparsionTest::greatherBfp_GreatherNegNeg()
+{
+    bfp value1, value2;
+    initInt(&value1, -10);
+    initInt(&value2, -20);
+    QCOMPARE(greatherBfp(&value1, &value2), true);
+}
+
+void ComparsionTest::greatherBfp_NotGreatherPosPos()
+{
+    bfp value1, value2;
+    initInt(&value1, 10);
+    initInt(&value2, 20);
+    QCOMPARE(greatherBfp(&value1, &value2), false);
+}
+void ComparsionTest::greatherBfp_NotGreatherNegPos()
+{
+    bfp value1, value2;
+    initInt(&value1, -10);
+    initInt(&value2, 20);
+    QCOMPARE(greatherBfp(&value1, &value2), false);
+}
+void ComparsionTest::greatherBfp_NotGreatherNegNeg()
+{
+    bfp value1, value2;
+    initInt(&value1, -20);
+    initInt(&value2, -10);
+    QCOMPARE(greatherBfp(&value1, &value2), false);
+}
+
+void ComparsionTest::greatherBfp_NanNumber()
+{
+    bfp value1, value2;
+    ValueHelper::assignNaN(&value1);
+    initInt(&value2, 10);
+    QCOMPARE(greatherBfp(&value1, &value2), false);
+}
+
+void ComparsionTest::greatherBfp_PosInfNumber()
+{
+    bfp value1, value2;
+    ValueHelper::assignPositiveInf(&value1);
+    initInt(&value2, 20);
+    QCOMPARE(greatherBfp(&value1, &value2), false);
+}
+
+void ComparsionTest::greatherBfp_NegInfNumber()
+{
+    bfp value1, value2;
+    ValueHelper::assignNegativeInf(&value1);
+    initInt(&value2, 20);
+    QCOMPARE(greatherBfp(&value1, &value2), false);
+}
+
+void ComparsionTest::greatherBfp_NanPosInf()
+{
+    bfp value1, value2;
+    ValueHelper::assignNaN(&value1);
+    ValueHelper::assignPositiveInf(&value2);
+    QCOMPARE(greatherBfp(&value1, &value2), false);
+}
+
+void ComparsionTest::greatherBfp_NanNegInf()
+{
+    bfp value1, value2;
+    ValueHelper::assignNaN(&value1);
+    ValueHelper::assignNegativeInf(&value2);
+    QCOMPARE(greatherBfp(&value1, &value2), false);
+}
+
+void ComparsionTest::greatherBfp_NanNan()
+{
+    bfp value1, value2;
+    ValueHelper::assignNaN(&value1);
+    ValueHelper::assignNaN(&value2);
+    QCOMPARE(greatherBfp(&value1, &value2), false);
+}
+
+void ComparsionTest::greatherBfp_PosInfPosInf()
+{
+    bfp value1, value2;
+    ValueHelper::assignPositiveInf(&value1);
+    ValueHelper::assignPositiveInf(&value2);
+    QCOMPARE(greatherBfp(&value1, &value2), false);
+}
+
+void ComparsionTest::greatherBfp_NegInfNegInf()
+{
+    bfp value1, value2;
+    ValueHelper::assignNegativeInf(&value1);
+    ValueHelper::assignNegativeInf(&value2);
+    QCOMPARE(greatherBfp(&value1, &value2), false);
+}
+
+void ComparsionTest::greatherBfp_PosInfNegInf()
+{
+    bfp value1, value2;
+    ValueHelper::assignPositiveInf(&value1);
+    ValueHelper::assignNegativeInf(&value2);
+    QCOMPARE(greatherBfp(&value1, &value2), false);
+}
+
+void ComparsionTest::lessBfp_LessPosPos()
+{
+    bfp value1, value2;
+    initInt(&value1, 10);
+    initInt(&value2, 20);
+    QCOMPARE(lessBfp(&value1, &value2), true);
+}
+
+void ComparsionTest::lessBfp_LessNegPos()
+{
+    bfp value1, value2;
+    initInt(&value1, -20);
+    initInt(&value2, 10);
+    QCOMPARE(lessBfp(&value1, &value2), true);
+}
+
+void ComparsionTest::lessBfp_LessNegNeg()
+{
+    bfp value1, value2;
+    initInt(&value1, -20);
+    initInt(&value2, -10);
+    QCOMPARE(lessBfp(&value1, &value2), true);
+}
+
+void ComparsionTest::lessBfp_NotLessPosPos()
+{
+    bfp value1, value2;
+    initInt(&value1, 20);
+    initInt(&value2, 10);
+    QCOMPARE(lessBfp(&value1, &value2), false);
+}
+void ComparsionTest::lessBfp_NotLessPosNeg()
+{
+    bfp value1, value2;
+    initInt(&value1, 10);
+    initInt(&value2, -20);
+    QCOMPARE(lessBfp(&value1, &value2), false);
+}
+void ComparsionTest::lessBfp_NotLessNegNeg()
+{
+    bfp value1, value2;
+    initInt(&value1, -10);
+    initInt(&value2, -20);
+    QCOMPARE(lessBfp(&value1, &value2), false);
+}
+
+void ComparsionTest::lessBfp_NanNumber()
+{
+    bfp value1, value2;
+    ValueHelper::assignNaN(&value1);
+    initInt(&value2, 10);
+    QCOMPARE(lessBfp(&value1, &value2), false);
+}
+
+void ComparsionTest::lessBfp_PosInfNumber()
+{
+    bfp value1, value2;
+    ValueHelper::assignPositiveInf(&value1);
+    initInt(&value2, 20);
+    QCOMPARE(lessBfp(&value1, &value2), false);
+}
+
+void ComparsionTest::lessBfp_NegInfNumber()
+{
+    bfp value1, value2;
+    ValueHelper::assignNegativeInf(&value1);
+    initInt(&value2, 20);
+    QCOMPARE(lessBfp(&value1, &value2), false);
+}
+
+void ComparsionTest::lessBfp_NanPosInf()
+{
+    bfp value1, value2;
+    ValueHelper::assignNaN(&value1);
+    ValueHelper::assignPositiveInf(&value2);
+    QCOMPARE(lessBfp(&value1, &value2), false);
+}
+
+void ComparsionTest::lessBfp_NanNegInf()
+{
+    bfp value1, value2;
+    ValueHelper::assignNaN(&value1);
+    ValueHelper::assignNegativeInf(&value2);
+    QCOMPARE(lessBfp(&value1, &value2), false);
+}
+
+void ComparsionTest::lessBfp_NanNan()
+{
+    bfp value1, value2;
+    ValueHelper::assignNaN(&value1);
+    ValueHelper::assignNaN(&value2);
+    QCOMPARE(lessBfp(&value1, &value2), false);
+}
+
+void ComparsionTest::lessBfp_PosInfPosInf()
+{
+    bfp value1, value2;
+    ValueHelper::assignPositiveInf(&value1);
+    ValueHelper::assignPositiveInf(&value2);
+    QCOMPARE(lessBfp(&value1, &value2), false);
+}
+
+void ComparsionTest::lessBfp_NegInfNegInf()
+{
+    bfp value1, value2;
+    ValueHelper::assignNegativeInf(&value1);
+    ValueHelper::assignNegativeInf(&value2);
+    QCOMPARE(lessBfp(&value1, &value2), false);
+}
+
+void ComparsionTest::lessBfp_PosInfNegInf()
+{
+    bfp value1, value2;
+    ValueHelper::assignPositiveInf(&value1);
+    ValueHelper::assignNegativeInf(&value2);
+    QCOMPARE(lessBfp(&value1, &value2), false);
+}
+
+void ComparsionTest::isNaN_Number()
+{
+    bfp value;
+    initInt(&value, 1);
+    QCOMPARE(isNaN(&value), false);
+}
+
+void ComparsionTest::isNaN_Zero()
+{
+    bfp value;
+    initZero(&value);
+    QCOMPARE(isNaN(&value), false);
+}
+
+void ComparsionTest::isNaN_Nan()
+{
+    bfp value;
+    ValueHelper::assignNaN(&value);
+    QCOMPARE(isNaN(&value), true);
+}
+
+void ComparsionTest::isNaN_PosInf()
+{
+    bfp value;
+    ValueHelper::assignPositiveInf(&value);
+    QCOMPARE(isNaN(&value), false);
+}
+
+void ComparsionTest::isNaN_NegInf()
+{
+    bfp value;
+    ValueHelper::assignNegativeInf(&value);
+    QCOMPARE(isNaN(&value), false);
+}
+
+void ComparsionTest::isNegativeInf_Number()
+{
+    bfp value;
+    initInt(&value, 1);
+    QCOMPARE(isNegativeInf(&value), false);
+}
+
+void ComparsionTest::isNegativeInf_Zero()
+{
+    bfp value;
+    initZero(&value);
+    QCOMPARE(isNegativeInf(&value), false);
+}
+
+void ComparsionTest::isNegativeInf_Nan()
+{
+    bfp value;
+    ValueHelper::assignNaN(&value);
+    QCOMPARE(isNegativeInf(&value), false);
+}
+
+void ComparsionTest::isNegativeInf_PosInf()
+{
+    bfp value;
+    ValueHelper::assignPositiveInf(&value);
+    QCOMPARE(isNegativeInf(&value), false);
+}
+
+void ComparsionTest::isNegativeInf_NegInf()
+{
+    bfp value;
+    ValueHelper::assignNegativeInf(&value);
+    QCOMPARE(isNegativeInf(&value), true);
+}
+
+void ComparsionTest::isPositiveInf_Number()
+{
+    bfp value;
+    initInt(&value, 1);
+    QCOMPARE(isPositiveInf(&value), false);
+}
+
+void ComparsionTest::isPositiveInf_Zero()
+{
+    bfp value;
+    initZero(&value);
+    QCOMPARE(isPositiveInf(&value), false);
+}
+
+void ComparsionTest::isPositiveInf_Nan()
+{
+    bfp value;
+    ValueHelper::assignNaN(&value);
+    QCOMPARE(isPositiveInf(&value), false);
+}
+
+void ComparsionTest::isPositiveInf_PosInf()
+{
+    bfp value;
+    ValueHelper::assignPositiveInf(&value);
+    QCOMPARE(isPositiveInf(&value), true);
+}
+
+void ComparsionTest::isPositiveInf_NegInf()
+{
+    bfp value;
+    ValueHelper::assignNegativeInf(&value);
+    QCOMPARE(isPositiveInf(&value), false);
+}
+
+void ComparsionTest::isZero_Number()
+{
+    bfp value;
+    initInt(&value, 1);
+    QCOMPARE(isZero(&value), false);
+}
+
+void ComparsionTest::isZero_Zero()
+{
+    bfp value;
+    initZero(&value);
+    QCOMPARE(isZero(&value), true);
+}
+
+void ComparsionTest::isZero_Nan()
+{
+    bfp value;
+    ValueHelper::assignNaN(&value);
+    QCOMPARE(isZero(&value), false);
+}
+
+void ComparsionTest::isZero_PosInf()
+{
+    bfp value;
+    ValueHelper::assignPositiveInf(&value);
+    QCOMPARE(isZero(&value), false);
+}
+
+void ComparsionTest::isZero_NegInf()
+{
+    bfp value;
+    ValueHelper::assignNegativeInf(&value);
+    QCOMPARE(isZero(&value), false);
 }
--- UnitTests/ComparsionTest/ComparsionTest.h
+++ UnitTests/ComparsionTest/ComparsionTest.h
@@ -12,17 +12,76 @@ class ComparsionTest : public QObject {
 public:
     ComparsionTest();
 private slots:
-    void isEqualBfp_Equal();
-    void isEqualBfp_NotEqual();
-    void isEqualBfp_NanNumber();
-    void isEqualBfp_PosInfNumber();
-    void isEqualBfp_NegInfNumber();
-    void isEqualBfp_NanPosInf();
-    void isEqualBfp_NanNegInf();
-    void isEqualBfp_NanNan();
-    void isEqualBfp_PosInfPosInf();
-    void isEqualBfp_NegInfNegInf();
-    void isEqualBfp_PosInfNegInf();
+    void equalBfp_EqualPos();
+    void equalBfp_EqualNeg();
+    void equalBfp_NotEqualPosPos();
+    void equalBfp_NotEqualPosNeg();
+    void equalBfp_NanNumber();
+    void equalBfp_PosInfNumber();
+    void equalBfp_NegInfNumber();
+    void equalBfp_NanPosInf();
+    void equalBfp_NanNegInf();
+    void equalBfp_NanNan();
+    void equalBfp_PosInfPosInf();
+    void equalBfp_NegInfNegInf();
+    void equalBfp_PosInfNegInf();
+
+    void greatherBfp_GreatherPosPos();
+    void greatherBfp_GreatherPosNeg();
+    void greatherBfp_GreatherNegNeg();
+    void greatherBfp_NotGreatherPosPos();
+    void greatherBfp_NotGreatherNegPos();
+    void greatherBfp_NotGreatherNegNeg();
+    void greatherBfp_NanNumber();
+    void greatherBfp_PosInfNumber();
+    void greatherBfp_NegInfNumber();
+    void greatherBfp_NanPosInf();
+    void greatherBfp_NanNegInf();
+    void greatherBfp_NanNan();
+    void greatherBfp_PosInfPosInf();
+    void greatherBfp_NegInfNegInf();
+    void greatherBfp_PosInfNegInf();
+
+    void lessBfp_LessPosPos();
+    void lessBfp_LessNegPos();
+    void lessBfp_LessNegNeg();
+    void lessBfp_NotLessPosPos();
+    void lessBfp_NotLessPosNeg();
+    void lessBfp_NotLessNegNeg();
+    void lessBfp_NanNumber();
+    void lessBfp_PosInfNumber();
+    void lessBfp_NegInfNumber();
+    void lessBfp_NanPosInf();
+    void lessBfp_NanNegInf();
+    void lessBfp_NanNan();
+    void lessBfp_PosInfPosInf();
+    void lessBfp_NegInfNegInf();
+    void lessBfp_PosInfNegInf();
+
+    void isNaN_Number();
+    void isNaN_Zero();
+    void isNaN_Nan();
+    void isNaN_PosInf();
+    void isNaN_NegInf();
+
+    void isNegativeInf_Number();
+    void isNegativeInf_Zero();
+    void isNegativeInf_Nan();
+    void isNegativeInf_PosInf();
+    void isNegativeInf_NegInf();
+
+    void isPositiveInf_Number();
+    void isPositiveInf_Zero();
+    void isPositiveInf_Nan();
+    void isPositiveInf_PosInf();
+    void isPositiveInf_NegInf();
+
+    void isZero_Number();
+    void isZero_Zero();
+    void isZero_Nan();
+    void isZero_PosInf();
+    void isZero_NegInf();
+
 };
 
 #endif // COMPARSIONTEST_H
