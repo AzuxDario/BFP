7b28e39c4d1696197bba6f051b29b93a8eed5c5e
AzuxDario <pawel_mibor95@tlen.pl>
2018-08-16 23:41:44 +0200

    Make value check helper

--- UnitTests/Helpers/ValueHelper.cpp
+++ UnitTests/Helpers/ValueHelper.cpp
@@ -34,3 +34,26 @@ void ValueHelper::assignNegativeInf(bfp* value)
         value->significant[i] = 0;
     }
 }
+
+bool ValueHelper::checkValue(bfp* value, Sign sign, int exponent, std::string significant)
+{
+    bool returnValue = true;
+
+    if(value->sign != static_cast<int>(sign))
+    {
+        returnValue = false;
+    }
+    if(value->exponent != exponent)
+    {
+        returnValue = false;
+    }
+    for(unsigned int i = 0; i < significant.length(); i++)
+    {
+        if(significant[i] - 0x30 != value->significant[i])
+        {
+            returnValue = false;
+        }
+    }
+
+    return returnValue;
+}
--- UnitTests/Helpers/ValueHelper.h
+++ UnitTests/Helpers/ValueHelper.h
@@ -1,16 +1,21 @@
 #ifndef VALUEHELPER_H
 #define VALUEHELPER_H
 
+#include "string"
 #include "BFP.h"
 
 class ValueHelper
 {
 public:
+
+    enum class Sign {plus = 0, minus = 1};
+
     ValueHelper();
 
     static void assignNaN(bfp* value);
     static void assignPositiveInf(bfp* value);
     static void assignNegativeInf(bfp* value);
+    static bool checkValue(bfp* value, Sign sign, int exponent, std::string significant);
 };
 
 #endif // VALUEHELPER_H
